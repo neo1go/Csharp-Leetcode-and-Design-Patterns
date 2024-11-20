f0eb236043acf9f800a92734f2c79360a0ad1dd2
neo1go <neo1go@gmx.de>
2024-11-20 12:34:41 +0100

    Kleine Ergänzungen

--- PrototypePattern/PrototypePattern/Program.cs
+++ PrototypePattern/PrototypePattern/Program.cs
@@ -4,13 +4,13 @@
 namespace PrototypePattern
 {
     //wird genutzt, um ein existierendes Objekt zu kopieren ohne das der Code von den konkreten Klassen abhängt.
-    //So können auch Kopien von Objekten erstellt werden die sich nicht komplett gleichen.
+    //So können auch Kopien von Objekten erstellt werden, die sich nicht komplett gleichen.
 
     public class Program
     {
         public static void Main(string[] args)
         {
-            //Originalobjekt erstellen
+            //Originalobjekt erstellen.
             ConcretePrototypeA original = new ConcretePrototypeA("Max", 23);
             //Klonen des Originalobjekt durch Aufrufen der Clone Methode
             //so dass man dann nicht mehr alle Werte beim Erzeugen des Objektes eingeben muss wie beim Original 
@@ -23,13 +23,14 @@ namespace PrototypePattern
     }
 
     //Das Interface enthält nur die abstrakte Methode Clone
-    public interface Prototype
+    public interface IPrototype
     {
-        public Prototype Clone();
+        public IPrototype Clone();
     }
 
     
-    public class ConcretePrototypeA : Prototype
+    //Hier wird das Interface implementiert,das die Methode "Clone" bereitstellt. 
+    public class ConcretePrototypeA : IPrototype
     {
         public string Name {  get; set; }
         public int Age { get; set; }
@@ -41,10 +42,12 @@ namespace PrototypePattern
             Age = age;
         }
 
-        //Reale Implementierung
-        public Prototype Clone()
-        {
-            return (Prototype)this.MemberwiseClone();// shallow copy, also nur die Referenzen werden kopiert - führt zu Seiteneffekten u.U.
+        //Reale Implementierung für die Methode,
+        //die ein tatsächliches Objekt vom Typ ConcretePrototypeA als Klon zurückgibt.
+        public IPrototype Clone()
+        {//Hier wird ein Objekt zurückgegeben, dessen Klasse das Interface IPrototype implementieren muss,
+         //d.h. das CroncretePrototypeA als Klon zurückgegeben werden kann, da diese Klasse IPrototype implementiert.
+            return (IPrototype)this.MemberwiseClone();// shallow copy, also nur die Referenzen werden kopiert - führt zu Seiteneffekten u.U.
         }
 
         public override string ToString()

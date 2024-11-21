b71e0a5409fb7f226a38fde377c62c25550356b3
neo1go <neo1go@gmx.de>
2024-11-21 16:31:22 +0100

    Eventhandler

--- EventHandlerÜbung/EventHandlerÜbung/Program.cs
+++ EventHandlerÜbung/EventHandlerÜbung/Program.cs
@@ -7,40 +7,52 @@
         public event EventHandler? MyEvent;
 
 
-        //Dies ist das TriggerEvent das MyEvent auslöst. Also wird das Event nur ausgelöst wenn es nicht null ist und invoked wird.
-        public void TriggerEvent()
+        //Dies ist der Abonnent
+        private static void MyClass_MyEvent(object? sender, EventArgs e)
         {
-            MyEvent?.Invoke(this, EventArgs.Empty);//Invoke - Aufruf
+            Console.WriteLine("\nDas Event wurde ausgelöst.\n");
         }
 
-        //Diese Methode entspricht der Signatur eines EventHandler und wird bei dem Eventaufruf ausgelöst an den es angehangen wurde.
-
-        private static void MyClass_MyEvent(object? sender, EventArgs e)//EventHandler
-        {
-            Console.WriteLine("Das Event wurde ausgelöst");
-        }
-
-        //Diese Methode fügt den EventHandler zum Event hinzu
+       // Diese Methode fügt den Abonnenten zum bis dahin leeren Event hinzu.
         public void RegisterEventHandler()
         {
             MyEvent += MyClass_MyEvent; //Abonnieren
+            Console.WriteLine("Abo wurde hinzugefügt mit +=");
         }
 
+        //Um Speicherleaks zu minimieren, sollten Abonnenten auch wieder abgemeldet werden
+        public void DropEventHandler()
+        {
+            MyEvent -= MyClass_MyEvent; //Abmelden
+            Console.WriteLine("Abo wurde beendet mit -=");
+        }
+
+        //Dies ist das TriggerEvent das MyEvent auslöst. Also wird das Event nur ausgelöst
+        //wenn es nicht null ist und invoked wird.
+        public void TriggerEvent()
+        {
+            MyEvent?.Invoke(this, EventArgs.Empty);//Invoke - Aufruf auf einem Klassenobjekt 
+        }
     }
 
     public class Progam
     {
         public static void Main(string[] args)
         {
-            var myClass = new MyClass();           //Objektinstanziierung
-            myClass.RegisterEventHandler();        //Hier muss dem Event eine EventHandler-Methode angefügt werden(Abonnieren)
-                                                   //,damit das Event nicht null ist.
-            //(Geht auch anonym)
+            var myClass = new MyClass();           
+            myClass.RegisterEventHandler();  //Abonnent(EventHandler) wird registriert (+=) bzw angehangen
+                                             //auf dem Objekt myClass
+                                                   
+           //  (Geht auch anonym)
            //  myClass.MyEvent += (sender, e) => Console.WriteLine("Event ausgelöst");
 
 
             myClass.TriggerEvent();                //Hiermit wird die ganze Eventkette ausgelöst,
                                                    //aber nur falls ein EventHandler abonniert ist.
+
+            //Abo beenden
+            myClass.DropEventHandler();
+
         }
 
        

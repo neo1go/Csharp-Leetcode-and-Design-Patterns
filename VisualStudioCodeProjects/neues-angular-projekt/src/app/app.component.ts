16a18a259c0775b53f185de441c2f43790c8481a
neo1go <neo1go@gmx.de>
2025-01-04 12:35:26 +0100

    Angular Lernen

--- VisualStudioCodeProjects/neues-angular-projekt/src/app/app.component.ts
+++ VisualStudioCodeProjects/neues-angular-projekt/src/app/app.component.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
     selector: 'app-root',
-    imports: [RouterOutlet],
+    imports: [RouterOutlet,HeaderComponent,FooterComponent],
     templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css']
+    styleUrls: ['./app.component.css'],
+   standalone:true,
 })
 export class AppComponent {
   title = 'neues-angular-projekt';

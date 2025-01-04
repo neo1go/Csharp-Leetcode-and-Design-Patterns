194ca14e46c3a42d6ef341473ab62e16e27bd321
neo1go <neo1go@gmx.de>
2025-01-04 09:28:52 +0100

    Update von angular

--- VisualStudioCodeProjects/neues-angular-projekt/src/app/app.component.ts
+++ VisualStudioCodeProjects/neues-angular-projekt/src/app/app.component.ts
@@ -1,13 +1,11 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  
+    selector: 'app-root',
+    imports: [RouterOutlet],
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'neues-angular-projekt';

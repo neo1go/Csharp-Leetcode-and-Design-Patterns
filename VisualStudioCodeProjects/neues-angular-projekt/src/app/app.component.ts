bfa24f8bb6f999aca650a8056626aebf6f620e01
neo1go <neo1go@gmx.de>
2025-01-07 12:40:58 +0100

    Browsersettings zu chrome geändert

--- VisualStudioCodeProjects/neues-angular-projekt/src/app/app.component.ts
+++ VisualStudioCodeProjects/neues-angular-projekt/src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './header/header.component';
+import { HeaderComponent } from './header/header.component';//Hier müssen die Komponenten importiert werden
 import { FooterComponent } from './footer/footer.component';
 
 @Component({
     selector: 'app-root',
-    imports: [RouterOutlet,HeaderComponent,FooterComponent],
+    imports: [RouterOutlet,HeaderComponent,FooterComponent], //Hier müssen die Komponenten importiert werden
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css'],
    standalone:true,

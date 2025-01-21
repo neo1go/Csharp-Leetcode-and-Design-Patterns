6f982496bdad62a8961c64a1ebc941c069698a84
neo1go <neo1go@gmx.de>
2025-01-21 18:49:12 +0100

    Quicksort bearbeitet

---
+++ LinqTraining/LinqTraining/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+public class Program
+{
+
+    public static void Main()
+    {
+        /*
+         int[] numbers =  [1,2,3,4,5,6,7,7,8,6,5,36];
+
+         var evenNumbers = from n in numbers 
+             where n%2 == 0 
+             select n; 
+
+         foreach (var num in evenNumbers)
+         {
+             Console.WriteLine(num);
+         }
+         */
+        //------------------------------------
+        /*
+        List<String> names = new List<string>{"Anna","Bob","Charlie"};
+        
+        var filteredNames = names.Where(n=> n.StartsWith("a",StringComparison.OrdinalIgnoreCase));
+        
+        foreach(var n in filteredNames)
+        {
+            Console.WriteLine(n);
+        }
+        */
+        //-------------------------------------
+        /*
+        Dictionary<int,string> data = new Dictionary<int,string>
+        {
+            {1,"One"},
+            {2,"Two"},
+            {3,"Three"}
+        };
+        
+        var keys = from k in data
+            where k.Value.StartsWith("t",StringComparison.OrdinalIgnoreCase)
+            select k.Key;
+            
+            
+            foreach(var key in keys)
+            {
+                Console.WriteLine(key);
+            }
+          */
+        //------------------------------------
+        /*
+        int[] numbers = {1,2,3,4,5,6,7,8};
+        var evenNumbers = numbers.Where(n=>n%2==0)
+            .Select(n=>n);
+            
+        foreach(var num in evenNumbers){
+        Console.WriteLine(num);
+        }
+        */
+        //--------------------------------------
+
+        int[] numbers = { 1, 3, 4, 5, 6, 7, 8, 9, 10, 2, 4 };
+
+        var query = from n in numbers
+                    where n % 2 == 0
+                    select n;
+
+        var sortedQuery = query.OrderBy(n => n);
+
+        foreach (var s in sortedQuery)
+        {
+            Console.WriteLine(s);
+        }
+
+    }
+}
---
+++ QuickSortAlgo/QuickSortAlgo/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+namespace QuickSortAlgo
+{
+
+
+    public  class Program
+    {
+        public static int[] QuickSort(int[] list)
+        {
+            if(list.Length <= 1)  //Abbruchbedingung
+            {
+                return list;
+            }
+
+            int pivot = list[0];  //pivot wird hier auf den ersten Wert gelegt. Dies ist kritisch bezüglich der Dauer der Suche.
+
+            int[] leftArray = Array.FindAll(list, x => x < pivot);  //alle Werte, die kleiner als pivot sind
+            int[] rightArray = Array.FindAll(list, x => x > pivot); //alle Werte, die größer als pivot sind
+
+            /*  Alternativ die normale Variante
+            List<int> leftList = new List<int>();
+            foreach (int number in list)
+            {
+                if (number < pivot)
+                    leftList.Add(number);
+            }
+            int[] leftArray = leftList.ToArray();
+
+             List<int> rightList = new List<int>();
+            foreach (int number in list)
+            {
+                if (number < pivot)
+                    rightList.Add(number);
+            }
+            int[] rightArray = rightList.ToArray();
+            */
+
+
+
+            leftArray = QuickSort(leftArray);  //hier wird solange rekursiv aufgerufen, bis length <=1 ist, also Abbruch.
+            rightArray = QuickSort(rightArray);
+
+            int[] result = new int[leftArray.Length + 1 + rightArray.Length];  //+1 für den pivot
+
+            leftArray.CopyTo(result, 0);  //mit CopyTo wird ein Array in ein anderes eingefügt ab dem Index der angegeben wird, hier 0;
+
+            result[leftArray.Length] = pivot; //Achtung Length und index sind nicht gleich.Length ist +1 im Vergleich zum
+                                              //Index. Deswegen wird der pivot an eine leere Stelle nach dem linken Array gesetzt.
+            rightArray.CopyTo(result, leftArray.Length + 1);
+
+            return result;
+        }
+
+
+        public static void Main() 
+        {
+            int[] list =[34, 5, 2, 132, 6, 7, 8, 34, 8, 8, 1, 2, 3, 4,];
+            int[] sortedList = QuickSort(list);
+
+            Console.WriteLine("Sortiertes Array: "+ String.Join(",",sortedList));
+
+        }
+
+    }
+
+
+}

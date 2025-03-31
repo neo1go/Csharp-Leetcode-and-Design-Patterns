499f391afb6e8d080f17a0b83f1eb455cec76f12
neo1go <neo1go@gmx.de>
2025-03-31 17:54:25 +0200

    Leetcode C# Übungen

---
+++ PartitionLLabels763/PartitionLLabels763/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace PartitionLabels763
+{
+    // Leetcode 763
+    // Es sollen Partitionen eines Strings zurückgeben werden.
+    // Bspl.:  "ababcbacadefegdehijhklij"
+    // Es wird vom ersten Buchstaben, in diesem Fall das a, bis zum letzten a ein max Partitionslänge ermittelt.
+    // Quasi eine Spannweite für die Partition. Da alle Werte innerhalb dieses Partitionsbereiches kleiner als
+    // die Position von a waren, gilt dies als eine Partition. Nun wird die nächste Partition erstellt.
+    // Hier wird es interessant da mit dem Wert von d ein Maxwert gesetzt wird. Es sit aber ein Buchstabe innerhalb dieses
+    // Bereiches, der den Maxwert erhöht, nämlich das e. Dadurch wird ein neuer Maxwert  gesetzt. Da jetzt kein größerer Wert
+    // mehr gesetzt wurde, gilt die Position von e als Ende der Partition und es wird eine neue Partition angefangen.....usw.
+    //
+    // 2tes Beispiel: "cdedcaba" -> 5,3  "cdedc" weil c bis c      und "aba" weil a bis a
+    public class Program
+    {
+
+        public List<int> PartitionLabels(string s)
+        {
+            Dictionary<Char, int> lastIndex = new Dictionary<char, int>();
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                lastIndex[s[i]] = i;  //hier wird der dict befüllt mit den Values der Buchstaben.
+            }
+
+            List<int> partitions = new List<int>();
+            int partitionEnd = 0;
+            int partitionStart = 0;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                partitionEnd = Math.Max(partitionEnd, lastIndex[s[i]]);
+
+                if (partitionEnd == i)
+                {
+                    partitions.Add(i - partitionStart + 1);
+                    partitionStart = i + 1;
+                }
+            }
+            return partitions;
+        }
+
+
+        public static void Main(string[] args)
+        {
+            Program program = new Program();
+            // string s = "ababcbacadefegdehijhklij";
+            string s = "cdedcaba";
+            List<int> result = program.PartitionLabels(s);
+
+            foreach (int r in result)
+            {
+                Console.Write(r + ", ");
+            }
+        }
+    }
+}
---
+++ ValidSquare367/ValidSquare367/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+namespace ValidSquare367 {
+
+    //Leetcode 367
+    //Es soll ohne sqrt Funktion herausgefunden werden, ob von einer Zahl die Wurzel gezogen werden kann.
+    //Bei der ersten Variante wird mittels binary search der Mittelpunkt des binary search quadriert,
+    //bis der Wert getroffen wird falls die Zahl eine echte Quadratzahl ist. Es wird immer jede Zahl
+    // überprüft.
+    //Bei der 2ten Variante wird immer auf den ungeraden Zahlen iteriert,da diese 
+    public class Program
+    {
+
+        public bool ValidSquare(int num)
+        {
+            /*  // Klassische Variante
+            bool result=false;
+
+            int[] nums = new int[num];
+            for (int i = 0; i < num; i++)
+            {
+                nums[i] = i + 1;
+            }
+
+            int left = nums[0];      //linker Pointer
+            int m = nums.Length/2;  //Mittelpunkt
+            int right = nums.Length; //recher Pointer
+            //läuft nur durch die Länge der Nummernrange
+            for(int i = 0; i<nums.Length;i++)
+            {
+                while( left<= right){
+                //trifft genau
+                 if(nums[m]*nums[m]==num){
+                     return true;
+                 }
+                //Quadrierung ist kleiner als Zahl
+                 else if(nums[m]*nums[m]<num)
+                 {             
+                    left=m+1;  
+                 }
+                //ist größer als Zahl
+                else
+                {             
+                    right=m-1;
+                }
+                    m=(left+right)/2;
+                }
+            }     
+            return result;
+            */
+            //Neue Variante 
+            //Sequenzlösung da jede Quadratwurzel minus vorherige Quadratwurzel die nächste 
+            //ungerade Zahl der Sequenz ergibt 1,4,9,16,25,36,49
+            int oddNumber = 1;
+            int sum = 1;
+            while (sum <= num)//nur solange bis Zahl erreicht wurde
+            {
+                if (sum == num)
+                {
+                    return true;
+                }
+                oddNumber += 2;//also wird sum zu 3,5,7,9 usw.
+              
+
+                //Der Trick ist, das jede Quadrahtzahl minus der vorherigen Quadrahtzahl eine ungerade Zahl
+                //einer aufsteigenden Sequenz ergibt.
+                //2^0=0 2^1=1 2^2=4 2^3=8 2^4=16 normale Quadratzahlen
+                //1-0=1, Quadrat 4-1=3, Quadrat 9-4=5, Quadrat 16-9=7, Quadrat 25-16=9, also Quadratwurzel minus vorherige Quadratwurzel
+                //ergibt immer die nächste ungerade Zahl: 1,3,5,7,9,11,13,15,17,19 usw
+                sum += oddNumber; //wenn die sum genau auf num trifft, ist es eine Quadratzahl
+            }
+            return false;
+        }
+
+
+        public static void Main(string[] args)
+        {
+            Program program = new Program();
+            int num = 16;
+            bool result = program.ValidSquare(num);
+            Console.WriteLine(result);
+        }
+    }
+
+
+
+}

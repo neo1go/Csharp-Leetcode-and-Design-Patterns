0df90a74c6664a945d868a684bbad1cd30e64df1
neo1go <neo1go@gmx.de>
2025-04-21 20:38:38 +0200

    GasStation Leetcode 134 und Iterator Pattern

---
+++ GasStation134/GasStation134/Program.cs
@@ -0,0 +1,59 @@
+﻿//Leetcode 134
+//Gas Station
+//Zyklisches Durchlaufen einer Collection mittels (startStation + i) % n
+public class Program
+{
+    public static int ValidStart(int[] gasStations, int[] consumption)
+    {
+        int n = gasStations.Length;  //Länge eines ganzen Zyklus
+        if (n != consumption.Length) // Ungültige Eingabe, wenn Längen nicht übereinstimmen.
+        {
+            Console.WriteLine("Die Arrays sind nicht gleich groß");
+            return -1;
+        }
+        if (consumption.Sum() > gasStations.Sum())  
+        {
+            Console.WriteLine("Der Verbrauch ist höher als der tankbare Sprit.");
+            return -1;
+        }
+
+        for (int startStation = 0; startStation < n; startStation++) // Es wird jedesmal die nächste Tankstelle als start genommen und ein
+                                                                     // ganzer Zyklus n iteriert.
+        {
+            int currentFuel = 0;
+
+            for (int i = 0; i < n; i++) //hier wird immer die gesamte Länge einmal iteriert.
+            {
+                int currentStationIndex = (startStation + i) % n;//das ist die Besonderheit für die Positionsbestimmung
+                currentFuel += gasStations[currentStationIndex] - consumption[currentStationIndex];
+
+                if (currentFuel < 0)
+                {
+                    break; // Tank wäre leer, also zum nächsten Startpunkt weiterziehen
+                }
+            }
+
+            // Wenn die innere Schleife komplett durchgelaufen ist,
+            // bedeutet das, dass die Reise von dieser startStation möglich war.
+            if (currentFuel >= 0)
+            {
+                Console.WriteLine("Es ist genug Sprit vorhanden.");
+                return startStation;
+            }
+        }
+
+        return -1;
+    }
+
+    public static void Main(string[] args)
+    {
+
+        int[] gasStations = [4, 2, 3, 4, 5];
+        int[] consumption = [3, 4, 5, 1, 2];
+
+        Console.WriteLine(ValidStart(gasStations, consumption));
+    }
+}
+
+
+
---
+++ IteratorPanncakeDiner/IteratorPanncakeDiner/Program.cs
@@ -0,0 +1,184 @@
+﻿using IteratorPancakeDiner;
+using System;
+using System.Collections;
+
+
+
+namespace IteratorPancakeDiner
+{
+    //Dieses interface stellt die Deklarationen für die konkreten Methoden bereit
+    public interface Iterator
+    {
+        bool hasNext();
+        MenuItem next();
+    }
+ //------------------------------------------------------
+    public class MenuItem
+    {
+        public string name;
+        public string description;
+        public bool vegetarian;
+        public double price;
+
+        public MenuItem(string name, string description, bool vegetarian, double price)
+        {
+            this.name = name;
+            this.description = description;
+            this.vegetarian = vegetarian;
+            this.price = price;
+        }
+    }
+//--------------------------------------------------------
+   
+    //Klasse für die Diner Menüliste, also die konkrete Collection, die iteriert werden soll
+    public class DinerMenu
+    {
+        const int MAX_ITEMS = 6;
+        int numberOfItems = 0;
+        MenuItem[] menuItems;
+
+        public DinerMenu()
+        {
+            menuItems = new MenuItem[MAX_ITEMS];
+            AddItem("Vegetarisches Sandwich", "(Falscher) Schinken mit Salat und Tomate auf Volkornbrot", true, 2.99);
+            AddItem("Schinkensandwich", "Schinken mit Salat und Tomate auf Volkornbrot", false, 2.99);
+            AddItem("Tagessuppe", "Tagessuppe mit Kartoffelsalat als Beilage", false, 3.29);
+            AddItem("HotDog", "HotDog mit Sauerkraut, Soße, Zwiebeln und Käse", false, 3.05);
+        }
+
+        public void AddItem(string name, string description, bool vegetarian, double price)
+        {
+            if (numberOfItems >= MAX_ITEMS)
+            {
+                Console.WriteLine("Speisekarte ist voll. Kann keine weiteren Gerichte hinzufügen.");
+            }
+            else
+            {
+                MenuItem newItem = new MenuItem(name, description, vegetarian, price);
+                menuItems[numberOfItems] = newItem;
+                numberOfItems++;
+            }
+        }
+
+        public Iterator CreateIterator()
+        {
+            return new DinerMenuIterator(menuItems);
+        }
+    }
+    //Klasse für das Pfannkuchenhaus Menü ,also die konkreten Collections die iteriert werden sollen
+    public class PancakeHouseMenu
+    {
+        ArrayList menuItems = new ArrayList();
+
+        public PancakeHouseMenu()
+        {
+            AddItem("Kürbispfannkuchen", "Pfannkuchen mit Kürbis und Ahornsirup", true, 3.99);
+            AddItem("Blaubeerpfannkuchen", "Pfannkuchen mit frischen Blaubeeren", true, 4.49);
+        }
+
+        public void AddItem(string name, string description, bool vegetarian, double price)
+        {
+            MenuItem item = new MenuItem(name, description, vegetarian, price);
+            menuItems.Add(item);
+        }
+
+        public Iterator CreateIterator()
+        {
+            return new PancakeHouseIterator(menuItems);
+        }
+    }
+
+    //----------------------------------------------------------------------
+
+    // Iterator speziell für DinerMenu Klasse und Array Iteration
+    public class DinerMenuIterator : Iterator
+    {
+        MenuItem[] items;  //dieser Iterator wird für eine Array-Iteration genutzt im Gegensatz zu pancake mit ArrayList
+        int position = 0;
+
+        public DinerMenuIterator(MenuItem[] items)
+        {
+            this.items = items;
+        }
+
+        public bool hasNext()
+        {
+            return position < items.Length && items[position] != null;
+        }
+
+        public MenuItem next()
+        {
+            MenuItem menuItem = items[position];
+            position++;
+            return menuItem;
+        }
+    }
+    //PancakeHouse Iterator, speziell für  ArrayList Collections
+    public class PancakeHouseIterator : Iterator
+    {
+        ArrayList items;  //hier wird der Iterator für eine ArrayList Collection erstellt
+        int position = 0;
+
+        public PancakeHouseIterator(ArrayList items)
+        {
+            this.items = items;
+        }
+
+        public bool hasNext()
+        {
+            return position < items.Count;
+        }
+
+        public MenuItem next()
+        {
+            MenuItem item = (MenuItem)items[position];
+            position++;
+            return item;
+        }
+    }
+
+    //Hier werden eigentlich beide Collections instanziiert und man erhält dann mittels waitress Objekt Zugriff darauf.
+    public class Waitress
+    {
+        PancakeHouseMenu pancakeHouseMenu;
+        DinerMenu dinerMenu;
+
+        public Waitress(PancakeHouseMenu pancakeHouseMenu, DinerMenu dinerMenu)
+        {
+            this.pancakeHouseMenu = pancakeHouseMenu;
+            this.dinerMenu = dinerMenu;
+        }
+
+        public void PrintMenu()
+        {
+            Console.WriteLine("SPEISEKARTE\n----\nFRÜHSTÜCK");
+            PrintMenuInternal(pancakeHouseMenu.CreateIterator());
+            Console.WriteLine("\nMITTAGESSEN");
+            PrintMenuInternal(dinerMenu.CreateIterator());
+        }
+
+        public void PrintMenuInternal(Iterator iterator)
+        {
+         while (iterator.hasNext())
+            {
+                MenuItem item = iterator.next();
+                Console.WriteLine($"{ item.name}, {item.price:0.00} €");
+                Console.WriteLine($"  ({item.description})");
+                Console.WriteLine($"  {(item.vegetarian ? "[Vegetarisch]" : "")}");
+            }
+        }
+    }
+}
+
+
+public class MenuTestDrive
+{
+    public static void Main(string[] args )
+    {
+        PancakeHouseMenu pan = new PancakeHouseMenu();
+        DinerMenu men = new DinerMenu();
+        Waitress waitress = new Waitress(pan, men);
+
+        waitress.PrintMenu();
+    }
+}

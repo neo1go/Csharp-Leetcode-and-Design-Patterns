a22218e7dc0a01ec6e6c952cb9fb77cf9c0f1311
neo1go <neo1go@gmx.de>
2025-05-02 11:52:20 +0200

    BuildingsWithAnOceanView1762 und ValidWordAbbreviation408

---
+++ BuildingsWithAnOceanView1762/BuildingsWithAnOceanView1762/Program.cs
@@ -0,0 +1,84 @@
+﻿
+
+
+// Leetcode 1762 Buildings with an Ocean View
+//
+// Es sollen nur die Indexe der Hotels im Array angezeigt werden, die Blick aufs Meer auf der rechten Seite haben.
+// Bspl.:
+// [4,2,3,1] -> 4 kann alles sehen und 3 sowie 1 können alles sehen und werden nicht blockiert in ihrem Blick.
+// Das Ergebnis lautet also [0,2,3].
+//
+// Die 2te Variante ist performanter, da sie nur eine Schleife nutzt und rückwärts iteriert.
+// Anschließend kann man mit stack oder der Reverse()-Funktion das Ergebnis rumdrehen.
+public class Program
+{
+    /*
+    public static int[] OceanIsVisible(int[] hotels)
+    {
+        List<int> result = new List<int>();//habe ich gewählt, um leicht zu erweitern
+      
+        
+        for (int i = 0; i<hotels.Length;i++)
+        {
+            bool hasView=true;
+            
+            for(int j = i+1;j<hotels.Length;j++)
+            {
+                
+                if(hotels[j]>=hotels[i])
+                {
+                   hasView=false;
+                    break;
+                }
+            }
+             if(hasView)
+        {
+            result.Add(i);
+        }
+            
+        }
+           
+        return result.ToArray();
+    }
+    
+   */
+    public static int[] OceanSuperPerformant(int[] hotels)
+    {
+        List<int> result = new List<int>();
+        int maxHeight = 0;
+        Stack<int> current = new Stack<int>();
+
+        for (int i = hotels.Length - 1; i >= 0; i--)
+        {
+            if (hotels[i] > maxHeight)
+            {
+                current.Push(i);
+                maxHeight = hotels[i];
+            }
+        }
+
+        while (current.Count > 0)
+        {
+            result.Add(current.Pop());
+        }
+
+        return result.ToArray();
+    }
+
+    public static void Main(string[] args)
+    {
+        //int [] hotels =[4,2,3,1];
+        int[] hotels = [3, 5, 4, 4, 3, 2, 1];
+
+        //int[] result = OceanIsVisible(hotels);
+
+        int[] result = OceanSuperPerformant(hotels);
+        foreach (var r in result)
+        {
+            Console.WriteLine(r);
+        }
+    }
+
+}
+
+
---
+++ ValidWordAbbreviation408/ValidWordAbbreviation408/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+// Leetcode 408
+// Es geht bei dieser Aufgabe darum, ein Wort mit einer Abbrivation 
+// (Abkürzung mit Zahlen als Platzhaltern) zu vergleichen und bei korrekter Abbrivation ein true 
+// zurückzugeben.
+// Führende Nullen sind nicht zulässig.
+// Bspl.:  "Hallo" -> "Ha2o" == true. Die 2 ersetzt die beiden l's im String.
+// 
+// Die Besonderheit ist hier das Auslesen des Int-Wertes aus dem string in dem while-Loop.
+// Es geht hier um die ASCII-Vergleiche. '0' ist immer der Basiswert 48, der dann subtrahiert wird,
+// um den tatsächlichen Wert zu erhalten.
+// Achtung !!!! Mittels der Subtraktion des ASCII-Wertes des strings mit dem ASCII Wert von '0' wird 
+// dann automatisch ein int erzeugt.
+// Bspl.: string '12'     '1'-'0' -> 49-48 = 1              also ASCII Wert minus ASCII Wert ergibt integer
+//                        '2'-'0' -> 50-48 = 2
+//
+// Umwandlung in eine zusammenhängende Dezimalzahl:    (wobei subLength am Anfang immer 0 sein muß.)
+// für das erste Zeichen      subLength = 0 * 10 + 1  = 1     (dieser Wert wird durch *10 dann zur führenden Zahl.)
+// für das zweite Zeichen     subLength = 1 * 10 + 2  = 12  und somit umgewandelt in eine komplette Dezimalzahl.
+
+public class Program
+{
+    public static bool Abbreviation(string word, string abbr)
+    {
+        int wordIndex = 0;
+        int abbrIndex = 0;
+
+        while (abbrIndex < abbr.Length && wordIndex < word.Length)
+        {
+            if (char.IsDigit(abbr[abbrIndex])) //wenn es eine Zahl ist
+            {
+                if (abbr[abbrIndex] == '0')
+                {
+                    return false;//bei führender Null sofort abbrechen. Nicht gestattet.
+                }
+                int subLength = 0; //hier muß immer wieder für jede neue Zahl auf null gesetzt werden.
+
+                while (abbrIndex < abbr.Length && char.IsDigit(abbr[abbrIndex]))//solange es eine Nummer ist 
+                                                                                //und noch nicht das Ende des Strings erreicht wurde.
+                                                                                //Es wird also die gesamte Zahl eingelesen durch das while.
+                {
+                    subLength = subLength * 10 + (abbr[abbrIndex] - '0');//Dadurch wird ein char in int umgewandelt.
+                    abbrIndex++;
+                }
+                wordIndex += subLength;//hier wird die Nummer zu dem anderen Pointer hinzuaddiert. 
+            }
+            else
+            {
+                //Buchstaben müssen exakt übereinstimmen und Länge muss stimmen.
+                if (wordIndex >= word.Length || word[wordIndex] != abbr[abbrIndex])
+                {
+                    return false;
+                }
+                wordIndex++;
+                abbrIndex++;
+            }
+        }
+        //Beide Strings müssen vollständig abgearbeitet sein.
+        return wordIndex == word.Length && abbrIndex == abbr.Length;
+    }
+
+    public static void Main(string[] args)
+    {
+        string word = "internationalization";
+        string abbr = "i12iz4n";
+
+        Console.WriteLine(Abbreviation(word, abbr));
+    }
+
+}
+
+
